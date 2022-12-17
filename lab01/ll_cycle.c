e463dd0beb6d250903114956890247b0fd7d8d8a
taylover2016 <taylover2016@yahoo.com>
2022-12-17 19:53:05 +0800

    Lab1

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -2,6 +2,38 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* your code here */
+    node* tortoise = head;
+    node* hare = head;
+
+    if (hare == NULL)
+    {
+        return 0;
+    }
+
+    hare = hare->next;
+
+    if (hare == NULL)
+    {
+        return 0;
+    }
+    else if (hare->next == NULL)
+    {
+        return 0;
+    }
+
+    tortoise = hare;
+    hare = hare->next;
+
+    while (hare != tortoise && hare->next != NULL && hare->next->next != NULL)
+    {
+        hare = hare->next->next;
+        tortoise = tortoise->next;
+    }
+    
+    if (hare == tortoise)
+    {
+        return 1;
+    }
+
     return 0;
 }
