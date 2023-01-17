6a86f8656b24a4598140054744e01801866cb3d0
taylover2016 <taylover2016@yahoo.com>
2023-01-17 14:37:22 +0800

    Lab3

--- lab03/factorial.s
+++ lab03/factorial.s
@@ -22,3 +22,23 @@ main:
 
 factorial:
     # YOUR CODE HERE
+    # Save any callee-saved registers
+    addi sp, sp, -8
+    sw ra, 4(sp)
+    sw s0, 0(sp)
+    # Main body of the function
+    addi s0, x0, 1
+    loop:
+        beq a0, x0, return
+        # The full version should use mul and mulh
+        mul s0, s0, a0
+        addi a0, a0, -1
+        j loop  # jump to loop
+        
+
+    return:
+        add a0, s0, x0
+        lw s0, 4(sp)
+        lw ra, 0(sp)
+        addi sp, sp, 8
+        jr ra
